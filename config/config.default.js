59e4d0b4537856ab1757c35ffff798a3f21f57da
zhangpn <zhangpn@isis.vanderbilt.edu>
2015-09-18 12:01:26 -0500

    removed test directory

--- config/config.default.js
+++ config/config.default.js
@@ -7,7 +7,7 @@ var config = require('webgme/config/config.default');
 
 config.server.port = 9091;
 
-config.plugin.basePaths.push('./src/plugins/Test');
+//config.plugin.basePaths.push('./src/plugins/Test');
 //config.plugin.basePaths = ['./src/plugins/Test']; //disable exposure of core-plugins
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_boilerplate';
