f04b87781059901dad135cd20f3aa18b961c790b
zhangpn <zhangpn@isis.vanderbilt.edu>
2015-09-28 09:43:23 -0500

    SysML diagrams update - use case and sequence diagram added

--- config/config.default.js
+++ config/config.default.js
@@ -10,6 +10,12 @@ config.server.port = 9091;
 //config.plugin.basePaths.push('./src/plugins/Test');
 //config.plugin.basePaths = ['./src/plugins/Test']; //disable exposure of core-plugins
 
-config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_boilerplate';
+
+config.client.defaultProject.name = "guest+SysML";
+config.client.defaultProject.branch = "master";
+config.client.defaultProject.node = "root";
+
+config.seedProjects.defaultProject = "SysML";
+config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_sysml';
 
 module.exports = config;
--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ var config = require('webgme/config/config.default'),
 
 // Overwrite options as needed
 config.server.port = 9091;
-config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_sysml';
+
 
 validateConfig(config);
 module.exports = config;
