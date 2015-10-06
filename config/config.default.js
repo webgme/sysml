7764ee1e584187884eb439b62300a2696850c084
zhangpn <zhangpn@isis.vanderbilt.edu>
2015-10-06 15:57:59 -0500

    updated default project config

--- config/config.default.js
+++ config/config.default.js
@@ -4,18 +4,21 @@
  */
 
 var config = require('webgme/config/config.default');
-
+var path = require('path');
 config.server.port = 9091;
 
 // config.plugin.basePaths.push('./src/plugins/Test');
 // config.plugin.basePaths = ['./src/plugins/Test']; //disable exposure of core-plugins
 
+if (config.client.defaultContext) {
+	config.client.defaultContext.project = "guest+SysML";
+} else {
+	config.seedProjects.defaultProject = "SysML";
+}
 
-// config.client.defaultProject.name = "guest+SysML";
-// config.client.defaultProject.branch = "master";
-// config.client.defaultProject.node = "root";
 
-// config.seedProjects.defaultProject = "SysML";
+config.visualization.decoratorPaths.push('./src/decorators');
+
 config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_sysml';
 
 module.exports = config;
