302fb108d60d14aa3883f6d03345b35d073f9451
Dana Zhang <zhangpn@isis.vanderbilt.edu>
2015-11-24 14:25:14 -0600

    remove jszip path in config

--- config/config.default.js
+++ config/config.default.js
@@ -20,8 +20,7 @@ config.seedProjects.basePaths.push('./seeds');
 
 config.requirejsPaths = {
 	ejs: "./node_modules/webgme/src/common/util/ejs",
-	xmljsonconverter: "./node_modules/webgme/src/common/util/xmljsonconverter",
-	jszip: './node_modules/webgme/node_modules/jszip/dist/jszip'
+	xmljsonconverter: "./node_modules/webgme/src/common/util/xmljsonconverter"
 };
 config.visualization.decoratorPaths.push('./src/decorators');
 config.visualization.decoratorsToPreload = null;
